cc36b565caddd45c339294b2782d4202421b8133
mohaiminsadiq@users.noreply.github.com <36579184>
2020-03-22 22:57:51 -0500

    Implemented sequential Delta Stepping

---
+++ DeltaSteppingSequential.java
@@ -0,0 +1,150 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Scanner;
+import java.util.Set;
+
+public class DeltaSteppingSequential {
+	static int delta = 3;
+	static int L; //the maximum shortest path weight, let's just call it largest weight*(number of vertices-1)
+	static int n;
+	static List<Integer> tent; //array of tentative distances, index is vertex
+	static List<Set<Integer>> buckets; //array of buckets i.e. sets of vertices
+	static List<HashMap<Integer,Integer>> adjList; //index of list is vertex number, mapping is <adjacent vertex, weight>
+	
+	private static List<Set<Integer>>initBuckets(){
+		List<Set<Integer>> buckets = new ArrayList<Set<Integer>>();
+		for (int i = 0; i < L/delta; i++) {
+			buckets.add(new HashSet<Integer>());
+		}
+		buckets.get(0).add(0); //add source vertex to B[0]
+		return buckets;		
+	}
+	
+	private static List<Integer> initTent(int n){
+		List<Integer> tent = new ArrayList<Integer>();
+		for (int i = 0; i < n; i++) {
+			tent.add(Integer.MAX_VALUE);
+		}
+		tent.set(0, 0);
+		return tent;
+	}
+	
+	public static void initVariables(String filename) throws FileNotFoundException {
+		createGraph(filename);
+		tent = initTent(n);
+		buckets = initBuckets();
+	}
+	
+	public static void findShortestPaths() {
+		int i = 0;
+		while ((i = NotEmpty(buckets)) >= 0) {
+			Set<Integer> R = new HashSet<>();
+			while (!buckets.get(i).isEmpty()) {
+				Map<Integer, Integer> reqs = findRequests(buckets.get(i), true);
+				R.addAll(buckets.get(i));
+				buckets.get(i).clear();
+				relaxRequests(reqs);
+			}
+			Map<Integer, Integer> reqs = findRequests(R, false);
+			relaxRequests(reqs);
+		}
+	}
+	
+	private static Integer NotEmpty(List<Set<Integer>> buckets) {
+		for (int i = 0; i < buckets.size(); i++) {
+			if (!buckets.get(i).isEmpty()) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	private static Map<Integer, Integer> findRequests(Set<Integer> V_prime, boolean light){
+		Map<Integer, Integer> reqs = new HashMap<Integer,Integer>();
+		for (Integer v: V_prime) { //for each vertex in the set V_prime
+			HashMap<Integer, Integer> v_edges = adjList.get(v); //get edges incident to v from adjacency list
+			for (Entry<Integer, Integer> entry : v_edges.entrySet()) {//for each edge incident to v
+				Integer w = entry.getKey(); //get vertex w adjacent to v
+				Integer d = entry.getValue(); //weight for edge (v,w)
+				if (light) {
+					if (d <= delta) {
+						reqs.put(w, tent.get(v) + d);
+					}
+				} else { //heavy
+					if (d > delta) {
+						reqs.put(w, tent.get(v) + d);
+					}
+				}
+				
+			}			
+		}
+		return reqs;
+	}
+	
+	private static void relaxRequests(Map<Integer, Integer> reqs) {
+		for (Entry<Integer, Integer> entry : reqs.entrySet()) {
+			relax(entry.getKey(), entry.getValue());
+		}
+	}
+	
+	private static void relax(int w, int x) {
+		if (x < tent.get(w)) {
+			if (tent.get(w) != Integer.MAX_VALUE) {
+				buckets.get(tent.get(w)/delta).remove(new Integer(w));
+			} 
+			buckets.get(x/delta).add(new Integer(w));
+			tent.set(w, x);
+ 		}
+	}
+	
+	private static void createGraph(String filename) throws FileNotFoundException {
+		File inFile = new File(filename);
+		Scanner in = new Scanner(inFile);
+		
+		//Find total number of vertices in the graph
+		int numVertices = 0;
+		String[] length = in.nextLine().trim().split("\\s+");
+		  for (int i = 0; i < length.length; i++) {
+		    numVertices++;
+		  }
+		in.close();
+		n = numVertices;
+		adjList = new ArrayList<>(n);
+		//Initialize adjacency list
+		for (int i = 0; i < numVertices; i++) {
+			adjList.add(new HashMap<Integer, Integer>());
+		}
+
+		in = new Scanner(inFile);
+		
+		//convert the adjacency matrix from the input file into an adjacency list
+		int vertex = 0;
+		int max = 0;
+		while (in.hasNextLine()) {
+		  String[] currentLine = in.nextLine().trim().split("\\s+"); 
+		     for (int i = 0; i < currentLine.length; i++) {
+		    	int weight = Integer.parseInt(currentLine[i]);
+		    	if (weight > 0) {
+		    		if (weight > max) max = weight;
+		    		adjList.get(vertex).put(i, weight);    
+		    	}
+		     }
+		     vertex++;
+		 } 
+		L = max*(n-1);
+	}
+
+	public static void main(String args[]) throws FileNotFoundException {
+		initVariables("inp.txt");
+		findShortestPaths();
+		for (int i = 0; i < n; i++) {
+			System.out.println("Vertex: " + i + " Path weight: " + tent.get(i));
+		}
+	}
+}
